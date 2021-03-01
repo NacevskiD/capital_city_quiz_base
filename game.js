45f813e45dc4e974ab77c4068a1ecc5404e73610
David <nacevskidavid@gmail.com>
2021-02-28 21:48:08 -0600

    Solved

--- game.js
+++ game.js
@@ -2,6 +2,7 @@ let randomCountryElement = document.querySelector('#random-country')
 let userAnswerElement = document.querySelector("#user-answer")
 let submitButton = document.querySelector("#submit-answer")
 let resultTextElement = document.querySelector('#result')
+let playAgain = document.querySelector('#play-again')
 
 // TODO finish the script to challenge the user about their knowledge of capital cities.
 // An array of country codes is provided in the countries.js file. 
@@ -10,24 +11,39 @@ let resultTextElement = document.querySelector('#result')
 
 console.log(countriesAndCodes)  // You don't need to log countriesAndCodes - just proving it is available 
 
+let randomNumber = Math.floor(Math.random() * countriesAndCodes.length)
+let randomCountryName = countriesAndCodes[randomNumber].name
+randomCountryElement.innerHTML = randomCountryName
 
-// TODO when the page loads, select an element at random from the countriesAndCodes array
+submitButton.addEventListener('click',function (){
+    let answer = userAnswerElement.value
+    let countryCode = countriesAndCodes[randomNumber]["alpha-2"]
 
-// TODO display the country's name in the randomCountryElement 
+    fetch(`https://api.worldbank.org/v2/country/${countryCode}?format=json`).then(result => result.json() )
+        .then( (countryData) =>{
 
-// TODO add a click event handler to the submitButton.  When the user clicks the button,
-//  * read the text from the userAnswerElement 
-//  * Use fetch() to make a call to the World Bank API with the two-letter country code (from countriesAndCodes, example 'CN' or 'AF')
-//  * Verify no errors were encountered in the API call. If an error occurs, display an alert message. 
-//  * If the API call was successful, extract the capital city from the World Bank API response.
-//  * Compare it to the user's answer. 
-//      You can decide how correct you require the user to be. At the minimum, the user's answer should be the same
-//      as the World Bank data - make the comparison case insensitive.
-//      If you want to be more flexible, include and use a string similarity library such as https://github.com/hiddentao/fast-levenshtein 
-//  * Finally, display an appropriate message in the resultTextElement to tell the user if they are right or wrong. 
-//      For example "Correct! The capital of Germany is Berlin" or "Wrong - the capital of Germany is not G, it is Berlin"
+            let countryCapital= countryData[1][0].capitalCity
+            answer = answer.toUpperCase()
+            countryCapital = countryCapital.toUpperCase()
+
+            if (answer === countryCapital){
+                resultTextElement.innerHTML = `ANSWER IS CORRECT! The Capital of ${randomCountryName} is ${answer}`
+            } else {
+                resultTextElement.innerHTML = `ANSWER IS FALSE! The Capital of ${randomCountryName} is not ${answer}, it's ${countryCapital}`
+            }
+
+        }).catch( (error) =>{
+            alert('Error occured' + error)
+    })
+})
+
+playAgain.addEventListener('click',function (){
+    randomNumber = Math.floor(Math.random() * countriesAndCodes.length)
+    randomCountryName = countriesAndCodes[randomNumber].name
+    randomCountryElement.innerHTML = randomCountryName
+    userAnswerElement.value = ''
+    resultTextElement.innerHTML = 'Replaced with result'
+})
 
 
-// TODO finally, connect the play again button. Clear the user's answer, select a new random country, 
-// display the country's name, handle the user's guess. If you didn't use functions in the code you've 
-// already written, you should refactor your code to use functions to avoid writing very similar code twice. 
+
